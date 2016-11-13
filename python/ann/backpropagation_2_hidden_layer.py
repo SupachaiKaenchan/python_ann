f9c6d57ebaa248ad4ac84c82af8829adbbf32926
dewtx29 <dewtx29@gmail.com>
2016-11-13 20:36:58 +0700

    trim wtf .py

--- python/ann/backpropagation_2_hidden_layer.py
+++ python/ann/backpropagation_2_hidden_layer.py
@@ -22,7 +22,7 @@ np.random.seed(1)
 syn0 = 2*np.random.random((3,4)) - 1
 syn1 = 2*np.random.random((4,1)) - 1
 
-for j in xrange(60000):
+for j in xrange(10000):
 
 	# Feed forward through layers 0, 1, and 2
     l0 = X
@@ -32,7 +32,7 @@ for j in xrange(60000):
     # how much did we miss the target value?
     l2_error = y - l2
     
-    if (j% 10000) == 0:
+    if (j% 1000) == 0:
         print "Error:" + str(np.mean(np.abs(l2_error)))
         
     # in what direction is the target value?
@@ -48,3 +48,6 @@ for j in xrange(60000):
 
     syn1 += l1.T.dot(l2_delta)
     syn0 += l0.T.dot(l1_delta)
+
+print "Output After Training:"
+print l2
