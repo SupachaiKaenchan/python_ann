5fe85a26d1a7d39c4c5e02d794dca4435f1236b7
dewtx29 <dewtx29@gmail.com>
2016-11-14 22:42:05 +0700

    add ffmpeg dem multifolder

---
+++ python/useful/ffmpeg_dem_multifolder.py
@@ -0,0 +1,72 @@
+# dewtx29@gmail.com
+# ffmpeg dem
+
+import os
+import os.path
+
+lst = [];
+
+count = 0
+
+sourcePath = "/home/d/videos/syncthing/dem_year"  #"/raid0/dem_year"
+desPath = "/raid0/bmp"
+
+
+for dirpath, dirnames, filenames in os.walk(sourcePath):
+    for filename in [f for f in filenames]:
+        count = count + 1;
+
+        tmpa = os.path.join(dirpath, filename)
+        tmpc = tmpa.rfind("/" )
+        tmpb = tmpa[tmpc+1:len(tmpa)]
+        tmpc = tmpb.replace(".dem","")
+        
+        lst.append(tmpc)
+        #print os.path.join(dirpath, filename);
+        #print count;
+
+
+print count
+
+lst = sorted(lst, key=str.lower)
+
+print len(lst)
+
+
+txt = open ("/home/d/2.sh","w")
+
+
+
+curPath = ""
+
+print "start"
+
+for folderloop in range (1,10):
+    txt.write("cd " + desPath + str(folderloop) + "\n")
+    
+    for i in range(0,count):
+     
+        a = lst[i]
+       # print i
+       # print a
+        
+
+       # print b
+
+       # print c
+       # print d
+
+        #ffmpeg -framerate 60 -i g1_%5d.bmp -c:v libx264 ~/g1.mp4
+       
+        e = "ffmpeg -framerate 60 -i " + a + "_%5d.bmp -c:v libx264 /raid0/"+ a + ".mp4"
+        
+        
+        #e = "mv '" + a + "'
+        #print e
+        txt.write(e + "\n")
+
+        txt.write("find . -name '" + a + "*' -delete\n")
+
+txt.close()
+
+print "done"
