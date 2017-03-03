8bc393194184075f245d1364df6e355d60e3214c
dewtx29 <dewtx29@gmail.com>
2017-03-03 22:44:48 +0700

    add new function , md5 not seem comparasion

--- python/useful/md5.py
+++ python/useful/md5.py
@@ -8,6 +8,113 @@ import re
 def special_match(strg, search=re.compile(r'[^a-z0-9.]').search):
     return not bool(search(strg))
 
+def be2column(pureString): 
+    words = []
+
+    for b in pureString:
+        if (b != ""):
+            words.append(b)
+            
+        if (len(words) > 3):
+            #print "..."
+            #print words
+            
+            newname = ""
+            for c in range (1,len(words)):
+                #print c
+                newname = newname + words[c]
+                
+                if (c < len(words)-1):
+                    newname = newname + " "
+                
+            #print pureString
+            #print newname
+            #print words
+
+            word2 = []
+            word2.append(words[0])
+            word2.append(newname)
+            #print "after !@#"
+
+            
+            #print word2
+            return word2
+        
+            #words = copy.copy(word2)
+
+    return words
+
+def columnTo2Column(pureColumn):
+    ret = []
+
+    for a in pureColumn:
+        tmp1 = a.split(" ");
+
+        
+        words = be2column(tmp1)
+        
+        ret.append(words)
+
+    return ret
+
+def isMD5Seem (md5C , slotC , md5L):
+    ret = False
+
+    tmpM = md5C[slotC][0]
+    
+    for j in range(0,len(md5L)):
+        tmpM2 = md5L[j][0]
+
+        if (tmpM == tmpM2):
+            ret = True
+            #print ("seem " + tmpM)
+
+    return ret
+
+def diffOfMD5(cloudPath , localPath):
+    fcloud = open(cloudPath, "r")
+    flocal = open(localPath, "r")
+
+    wNotSeem = open("/ram2/notSeem.sh" , "w")
+
+    linesC = fcloud.readlines()
+    linesL = flocal.readlines()
+
+    print linesC[0]
+    print linesL[0]
+
+    md5C = columnTo2Column(linesC)
+    md5L = columnTo2Column(linesL)
+
+    print md5C[1]
+
+    seemCount = 0;
+    notSeemCount = 0;
+    for i in range(0, len(md5C)):
+        if (i > 1000 ):
+            break
+        
+        tmpSeem = isMD5Seem(md5C , i , md5L)
+        if (tmpSeem == True):
+            seemCount = seemCount + 1
+        else:
+            notSeemCount = notSeemCount + 1
+            wNotSeem.write(md5C[i][0] + " " + md5C[i][1] + "\n")
+        #print tmpM
+
+
+    print str(seemCount) + " / " + str(notSeemCount)
+    
+    
+    print (str(len(md5C)) + " / " + str(len(md5L)))
+
+    print (str(len(linesC)) + " / " + str(len(linesL)))
+
+    flocal.close()
+    fcloud.close()
+    wNotSeem.close()
+
+
 def checkNonUnicode(filePath):
     f = open(filePath, "r")
 
@@ -121,13 +228,10 @@ def renameSpaceToUnderscore(filePath):
     #print tmp_b[0]
 
 
-
-    #f = open('/ram2/g_pat.txt', "r")
-    #f = open('/ram2/one_pat.txt', "r")
-    #f = open('/ram2/one_dew.txt', "r")
-    #f = open('/ram2/one_dd.txt', "r")
-    #f = open('/ram2/g_pat_photo.txt', "r")
 #checkNonUnicode("/ram2/g_dd.txt")
-renameSpaceToUnderscore("/ram2/one_dew.txt")
-
+#renameSpaceToUnderscore("/ram2/one_dew.txt")
+    
+diffOfMD5("/ram2/x.txt" , "/ram2/local.txt")
+          
+          
 
