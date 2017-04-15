e441e7c68194eff5917775d366c5bbb78ad440f5
dewtx29 <dewtx29@gmail.com>
2017-04-15 09:29:45 +0700

    bucket video add auto concat script

--- python/useful/bucketVideoByDuration.py
+++ python/useful/bucketVideoByDuration.py
@@ -25,6 +25,8 @@ tmpGroup = 1
 
 txt = open ("/ram/2.sh","w")
 
+listInGroup = []
+
 for tmp in lines:
 
     ss = tmp.split(' ')
@@ -38,7 +40,30 @@ for tmp in lines:
     if (tmpDura > maxDura):
         print tmpDura
         tmpDura = 0
+
+
+        fName = str(tmpGroup) + "_list.txt"
+        saveL = open ("/ram/tmp/" + fName,"w")
+
+        print ("listInGroup " + str(len(listInGroup)))
+        for gg in listInGroup:
+            saveL.write("file '" + gg + "'\n")
+            
+        saveL.close()
+
+
+
+
+        saveL2 = open ("/ram/tmp/" + str(tmpGroup) + "_bash.sh","w")
+        saveL2.write("ffmpeg -f concat -i " + fName + " -c copy /ram/tmp/" + str(listInGroup[0]) )
+        
+        saveL2.close()
+        
         tmpGroup = tmpGroup + 1
+        listInGroup = []
+
+    listInGroup.append(ss[0])
+    
 
     txt.write ("mkdir " + desPath + "/" + str(tmpGroup) +"\n")
     txt.write ("mv " + sourcePath + "/" + ss[0] + " " + desPath + "/" + str(tmpGroup) + "/"  + ss[0] + "\n")
