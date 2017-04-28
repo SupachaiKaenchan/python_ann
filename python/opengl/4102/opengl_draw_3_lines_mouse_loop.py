25d62f61ce1c665e30d6ad81c263ab0dfea5f4ce
dewtx29 <dewtx29@gmail.com>
2017-04-28 15:02:35 +0700

    move renderPls to be class

--- python/opengl/4102/opengl_draw_3_lines_mouse_loop.py
+++ python/opengl/4102/opengl_draw_3_lines_mouse_loop.py
@@ -11,83 +11,93 @@ name = 'ball_glut'
 dotList = []
 
 def main():
-    glutInit(sys.argv)
-    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH)
-    glutInitWindowSize(800, 600)
-    glutCreateWindow(name)
-
-    glClearColor(0.,0.,0.,1.)
-    glutDisplayFunc(display)
-    gluOrtho2D(0, 800, 600, 0);
-    glMatrixMode(GL_MODELVIEW);
-    glPushMatrix()
-
-    glutMouseFunc(mouseEvent);
-
-    #while(True):
-        #display()
-        #print ("looping")
-
-    thread1 = myThread(1, "Thread-1", 1)
-
-    # Start new Threads
-    thread1.start()
+    print "main hi"
+    tmpRen = RenderPls()
+    tmpRen.setupVariable()
     
-
-    glutMainLoop()
-
-
-    return
-
-
-def mouseEvent( button, state,  x,  y):
-    print ("mouse Event " )
-    mouseButton = button;
-    if button == GLUT_LEFT_BUTTON:    
-        if(state == GLUT_DOWN):
-            oldX = x;
-            oldY = y;
-            print ("click " + str(x) + " ," + str(y))
-
-            tmpC = [x,y]
-            dotList.append(tmpC)
-            
-
-
-def display():
-    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
-    glPushMatrix()
-    glLineWidth(2.5);     
-    glBegin(GL_LINES);
-    # left line
-    glColor3f(1.0, 0.0, 0.0);
-
-    a = random.random() * 800;
-    
-    glVertex2f(-400, 300);
-    glVertex2f(400, 300);
-    # right line
-    glColor3f(0.0, 1.0, 0.0);
-    glVertex2f(400, 300);
-    glVertex2f(800, 300);
-    
-    # top line
-    glColor3f(0.0, 0.0, 1.0);
-    glVertex2f(a, 0);
-    glVertex2f(a, 300);
-
-    for lop in range(0,len(dotList)):
-        tmpX = dotList[lop][0];
-        tmpY = dotList[lop][1];
-
-        glVertex2f(tmpX , tmpY)
+class RenderPls ():
+    def __init__(self):
+        print "RenderPls init"
         
-    
-    
-    glEnd();
-    glPopMatrix()
-    glutSwapBuffers()
-    return
+        
+    def setupVariable(self):
+        glutInit(sys.argv)
+        glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH)
+        glutInitWindowSize(800, 600)
+        glutCreateWindow(name)
+
+        glClearColor(0.,0.,0.,1.)
+        glutDisplayFunc(self.display)
+        gluOrtho2D(0, 800, 600, 0);
+        glMatrixMode(GL_MODELVIEW);
+        glPushMatrix()
+
+        glutMouseFunc(self.mouseEvent);
+
+        #while(True):
+            #display()
+            #print ("looping")
+
+        thread1 = myThread(1, "Thread-1", 1)
+
+        # Start new Threads
+        thread1.start()
+        
+
+        glutMainLoop()
+
+
+        return
+
+
+    def mouseEvent(self, button, state,  x,  y):
+        print ("mouse Event " )
+        mouseButton = button;
+        if button == GLUT_LEFT_BUTTON:    
+            if(state == GLUT_DOWN):
+                oldX = x;
+                oldY = y;
+                print ("click " + str(x) + " ," + str(y))
+
+                tmpC = [x,y]
+                dotList.append(tmpC)
+                
+
+
+    def display(self):
+        glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
+        glPushMatrix()
+        glLineWidth(2.5);     
+        glBegin(GL_LINES);
+        # left line
+        glColor3f(1.0, 0.0, 0.0);
+
+        a = random.random() * 800;
+        
+        glVertex2f(-400, 300);
+        glVertex2f(400, 300);
+        # right line
+        glColor3f(0.0, 1.0, 0.0);
+        glVertex2f(400, 300);
+        glVertex2f(800, 300);
+        
+        # top line
+        glColor3f(0.0, 0.0, 1.0);
+        glVertex2f(a, 0);
+        glVertex2f(a, 300);
+
+        for lop in range(0,len(dotList)):
+            tmpX = dotList[lop][0];
+            tmpY = dotList[lop][1];
+
+            glVertex2f(tmpX , tmpY)
+            
+        
+        
+        glEnd();
+        glPopMatrix()
+        glutSwapBuffers()
+        return
 
 class myThread (threading.Thread):
     def __init__(self, threadID, name, counter):
