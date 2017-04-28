df707ae637a613130576c221de6a40ce149d1a98
dewtx29 <dewtx29@gmail.com>
2017-04-28 13:59:00 +0700

    opengl draw 3 line

---
+++ python/opengl/opengl_draw_3_lines.py
@@ -0,0 +1,48 @@
+from OpenGL.GLUT import *
+from OpenGL.GLU import *
+from OpenGL.GL import *
+import sys
+
+name = 'ball_glut'
+
+def main():
+    glutInit(sys.argv)
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH)
+    glutInitWindowSize(800, 600)
+    glutCreateWindow(name)
+
+    glClearColor(0.,0.,0.,1.)
+    glutDisplayFunc(display)
+    gluOrtho2D(0, 800, 600, 0);
+    glMatrixMode(GL_MODELVIEW);
+    glPushMatrix()
+    glutMainLoop()
+    return
+
+
+
+def display():
+    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
+    glPushMatrix()
+    glLineWidth(2.5);     
+    glBegin(GL_LINES);
+    # left line
+    glColor3f(1.0, 0.0, 0.0);
+    glVertex2f(-400, 300);
+    glVertex2f(400, 300);
+    # right line
+    glColor3f(0.0, 1.0, 0.0);
+    glVertex2f(400, 300);
+    glVertex2f(800, 300);
+    
+    # top line
+    glColor3f(0.0, 0.0, 1.0);
+    glVertex2f(400, 0);
+    glVertex2f(400, 300);
+    
+    glEnd();
+    glPopMatrix()
+    glutSwapBuffers()
+    return
+
+if __name__ == '__main__': main()
--- python/opengl/opengl_mouse_event.py
+++ python/opengl/opengl_mouse_event.py
@@ -4,6 +4,8 @@ from pygame.locals import *
 from OpenGL.GL import *
 from OpenGL.GLU import *
 
+dotList = []
+
 verticies = (
     (1, -1, -1),
     (1, 1, -1),
@@ -41,6 +43,7 @@ def Cube():
 def drawLine():
     SIZE_W = 500
     SIZE_MX = 500.0
+    
     glClear(GL_COLOR_BUFFER_BIT);
     glColor3f(1.0, 1.0, 1.0);
     glBegin(GL_LINES);
@@ -50,6 +53,34 @@ def drawLine():
     glVertex2f(0.0, SIZE_MX);
     glEnd();
 
+def drawLineDot():
+    SIZE_W = 500
+    SIZE_MX = 500.0
+    
+    glClear(GL_COLOR_BUFFER_BIT);
+    glColor3f(1.0, 1.0, 1.0);
+    glBegin(GL_LINES);
+
+    
+    glVertex2f(-SIZE_MX, 0.0);
+    glVertex2f(SIZE_MX, 0.0);
+    #glVertex2f(0.0, -SIZE_MX);
+    #glVertex2f(0.0, SIZE_MX);
+
+    '''
+    for lop in range(0,len(dotList)):
+        print (str(lop) + " = " + str(dotList[lop]))
+
+        p1 = float(dotList[lop][0])
+        p2 = float(dotList[lop][1])
+
+        print ("p1 p2 : " + str(p1) + " , " + str(p2)) 
+        
+        glVertex2f(p1,p2)
+        
+    '''    
+    glEnd();
+
 
 
 def main():
@@ -57,9 +88,9 @@ def main():
     display = (800,600)
     pygame.display.set_mode(display, DOUBLEBUF|OPENGL)
 
-    gluPerspective(45, (display[0]/display[1]), 0.1, 50.0)
+    gluPerspective(0, (display[0]/display[1]), 0.1, 50.0)
 
-    glTranslatef(0.0,0.0, -5)
+    #glTranslatef(0.0,0.0, -5)
 
     while True:
         for event in pygame.event.get():
@@ -71,13 +102,15 @@ def main():
                 pos = pygame.mouse.get_pos()
                 ## check if cursor is on button ##
                 print ("mouse get pressed event : "  + str(pos))
+                dotList.append(pos);
 
         #glRotatef(1, 3, 1, 1)
-        glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
+        #glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
 
 
         #Cube()
-        drawLine()
+        #drawLine()
+        drawLineDot()
         
         pygame.display.flip()
         pygame.time.wait(10)
