e83463d43f33be311e41c38fca53c1594496bd54
dewtx29 <dewtx29@gmail.com>
2017-05-19 22:41:35 +0700

    gausian with numpy beta version

--- python/project/cos3101/gausian.py
+++ python/project/cos3101/gausian.py
@@ -1,29 +1,82 @@
-gau = []
-output = []
+import numpy as np
 
-a  = [2 , 4 , -2 ]
-gau.append(a)
+matrix1 = np.array([[2.0 , 4.0 ]
+                   ,[3.0 , -4.0 ]])
 
-a  = [4 , 9 , -3 ]
-gau.append(a)
+output = np.array([[6],[-1]])
+    
 
-a  = [-2 , -3 , 7 ]
-gau.append(a)
-
-
-b = [2]
-output.append(b)
-
-
-b = [8]
-output.append(b)
-
-
-b = [10]
-output.append(b)
-
-
-
-print gau
+print matrix1
 print output
 
+
+rowSize = len(matrix1)
+columnSize = len(matrix1[0])
+
+print ""
+print "r,c size " , rowSize, columnSize
+print ""
+
+for i in range (1 , rowSize):
+    for j in range (0,i):
+        print "loop " , i,j
+        print ""
+
+        print "matrix1[i] , tmpO1 " , matrix1[i] , output[i]
+
+
+        #m =  matrix1[i-1][j] / matrix1[i][j]
+
+        m =   matrix1[i][j] / matrix1[i-1][j] 
+
+
+
+        print "m old / cur" , matrix1[i-1][j] , " / " , matrix1[i][j]
+        
+        print m
+        print "m " , m
+        print ""
+
+       
+        g1 = np.dot(matrix1[i-1],  m)
+        g2 = np.dot(output[i-1] , m)
+            
+            
+        print "g1  " , g1 , " g2 " , g2
+    
+
+        print "matrix i " , matrix1[i]
+        print "matrix i-1 " , matrix1[i-1]
+        
+        # sub track
+
+        g3 = np.subtract(g1 , matrix1[i])
+        print "g3 ",g3
+
+        g4  = np.subtract(g2 , output[i])
+        print "g4 " , g4
+
+        
+        matrix1[i] = g3
+        output[i] = g4
+
+        print ""
+        print ">>> " , matrix1, " output " ,  output 
+        print ""
+
+print ""
+print "after " , matrix1, " output " ,  output
+
+
+# find out z y x
+
+ttY = output[1][0] / matrix1[1][1]
+
+print "new Y " ,ttY
+ttX =  (output[0][0] -  (matrix1[0][1]*ttY) ) / matrix1[0][0]
+print "new X " , ttX , output[0][0] ,  (matrix1[0][1]*ttY) , matrix1[0][0]
+print "output[0][0] /  (matrix1[0][1]*ttY)" , output[0][0] -  (matrix1[0][1]*ttY)
+
+
+testValue =  (matrix1[0][0] * ttX  ) + (matrix1[0][1] * ttY)
+print testValue , (matrix1[0][0] * ttX  ) ,(matrix1[0][1] * ttY)
---
+++ python/project/cos3101/numpy_test.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+a = np.array([1,3,4])
+print a
+
+
+b = [1,3,4]
+print b
